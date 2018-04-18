9ba83e63aa8f9ecd2ab5be4e513e3c0e05115ed5
alkraeuchi <alkraeuchi@users.noreply.github.com>
2018-04-19 01:16:27 +0200

    add dist folder

---
+++ dist/assets/bubbleChart/config/centers.js
@@ -0,0 +1,8 @@
+const CENTERS = {
+  instrument: [
+    {title: "Initiative", center: 0.9},
+    {title: "Motion", center: 0.63333},
+    {title: "Anzug", center: 0.36666},
+    {title: "Petition", center: 0.1}
+  ]
+};
