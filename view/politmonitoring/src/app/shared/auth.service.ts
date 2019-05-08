92de83c069d51a6dc9a80bda834bc4bebe7a6a04
alkraeuchi <alkraeuchi@users.noreply.github.com>
2019-05-08 17:21:28 +0200

    Upgrade to Angular@latest; Update dependencies;

--- view/politmonitoring/src/app/shared/auth.service.ts
+++ view/politmonitoring/src/app/shared/auth.service.ts
@@ -1,8 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from '../../environments/environment';
-import { Observable } from 'rxjs/Observable';
-import { BehaviorSubject } from 'rxjs/BehaviorSubject';
+import { Observable ,  BehaviorSubject } from 'rxjs';
 
 @Injectable()
 export class AuthService {
