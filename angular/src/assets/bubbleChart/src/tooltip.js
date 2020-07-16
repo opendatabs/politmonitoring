229b0bc9972bc4b8e1ef64ee480181882d6c8b5a
Sabine Brunner <sabine.brunner@inf.unibe.ch>
2020-07-16 10:21:17 +0200

    fix tooltip

--- angular/src/assets/bubbleChart/src/tooltip.js
+++ angular/src/assets/bubbleChart/src/tooltip.js
@@ -47,26 +47,24 @@ function floatingTooltip(tooltipId, width) {
    * based on d3 mouse event.
    */
   function updatePosition(event) {
-    var xOffset = 15;
-    var yOffset = 15;
+    var xOffset = 10;
+    var yOffset = 10;
 
-    var ttw = tt.style('width');
-    var tth = tt.style('height');
+    var ttw = parseInt(tt.style('width'),10);
+    var tth = parseInt(tt.style('height'),10);
 
     var wscrY = window.scrollY;
     var wscrX = window.scrollX;
 
-    var curX = (document.all) ? event.clientX + wscrX : event.pageX;
-    var curY = (document.all) ? event.clientY + wscrY : event.pageY;
-    var ttleft = ((curX - wscrX + xOffset * 2 + ttw) > window.innerWidth) ?
-                 curX - ttw - xOffset * 2 : curX + xOffset;
+    var curX = document.all ? event.clientX + wscrX : event.pageX;
+    var curY = document.all ? event.clientY + wscrY : event.pageY;
+    var ttleft = curX - wscrX + xOffset * 2 + ttw > window.innerWidth ? curX - ttw - xOffset : curX + xOffset;
 
     if (ttleft < wscrX + xOffset) {
       ttleft = wscrX + xOffset;
     }
 
-    var tttop = ((curY - wscrY + yOffset * 2 + tth) > window.innerHeight) ?
-                curY - tth - yOffset * 2 : curY + yOffset;
+    var tttop = curY - wscrY + yOffset * 2 + tth > window.innerHeight ? curY - tth - yOffset : curY + yOffset;
 
     if (tttop < wscrY + yOffset) {
       tttop = curY + yOffset;
