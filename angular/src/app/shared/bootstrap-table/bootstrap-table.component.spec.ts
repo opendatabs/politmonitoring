8637c68e9a55793e800ac6f8eadd8f30f1849abe
Roland <roland.widmer@inf.unibe.ch>
2021-06-08 09:54:48 +0200

    angular 11

--- angular/src/app/shared/bootstrap-table/bootstrap-table.component.spec.ts
+++ angular/src/app/shared/bootstrap-table/bootstrap-table.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { BootstrapTableComponent } from './bootstrap-table.component';
 
@@ -6,7 +6,7 @@ describe('BootstrapTableComponent', () => {
   let component: BootstrapTableComponent;
   let fixture: ComponentFixture<BootstrapTableComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ BootstrapTableComponent ]
     })
